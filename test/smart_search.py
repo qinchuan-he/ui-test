32e5c2cc4b6b637d38025fc7c46f05fec28c054d
qinchuan.he <qinchuan.he@fir.ai>
2020-01-15 10:23:41 +0800

    关闭不必要的执行

--- test/smart_search.py
+++ test/smart_search.py
@@ -134,11 +134,11 @@ class search_result(object):
 
 
 
-mode = 2
-driver = execBrower(mode)
-User().login(driver)
-search_home().lately_collection(driver)
-# search_home().my_subscribe(driver)
+# mode = 2
+# driver = execBrower(mode)
+# User().login(driver)
+# search_home().lately_collection(driver)
+# # search_home().my_subscribe(driver)
 # search_home().my_annotation(driver)
 # search_result().search(driver)
 # search_result().check_jmeter(driver,"http://192.168.1.49:8080/jmeter/report/index.html")
