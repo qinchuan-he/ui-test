b472f988d51a4a53597a2fa0115425ce9cc2517c
qinchuan.he <qinchuan.he@fir.ai>
2020-10-09 16:12:20 +0800

    Linux执行路径调整

--- test/check_5pm.py
+++ test/check_5pm.py
@@ -1,6 +1,11 @@
 # coding = utf-8
 
+import sys
+import os
 
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.insert(0, rootPath)
 
 # 下午5点执行的定时任务
 from test.check_mysql import check_notice
